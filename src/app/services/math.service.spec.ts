479afa71a1be922fbb9824834579befedb0f051c
RahmaSamyITS <Rahma.Samy@egypt-its.com>
2025-04-26 17:45:52 +0300

    s

--- src/app/services/math.service.spec.ts
+++ src/app/services/math.service.spec.ts
@@ -10,6 +10,6 @@ describe('MathService', () => {
   it('should add two numbers correctly', () => {
     const result = service.add(2, 3);
     expect(result).toBe(5);
-    console.log('✅ MathService Add Function Test Passed: 2 + 3 =', result);
+    console.log('✅ MathSxdervice Add Function Test Passed: 2 + 3 =', result);
   });
 });
